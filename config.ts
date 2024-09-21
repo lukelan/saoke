640468ff58ae741e6a8d7dd5bc0c36b1ca52446c
trongv <vuminh.trong@gmail.com>
2024-09-21 16:19:34 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,13 @@
 import "jsr:@std/dotenv/load";
 
+//export const config = {
+//  dbUrl: Deno.env.get("TURSO_DATABASE_URL")!,
+//  dbAuthToken: Deno.env.get("TURSO_AUTH_TOKEN")!,
+//  env: Deno.env.get("ENV")!,
+//};
+
 export const config = {
-  dbUrl: Deno.env.get("TURSO_DATABASE_URL")!,
-  dbAuthToken: Deno.env.get("TURSO_AUTH_TOKEN")!,
+  dbUrl: "libsql://saoke-mttq-vuminhkhang009.turso.io",
+  dbAuthToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjY3MTYwODksImlkIjoiMjJhMGY4ZjctMWIyZC00MGY0LTgwYjYtMTgxN2M0OGExZTIzIn0.ir1jjCBPwLCjSwiYyZv6Na14Zv55qgnNpEgTlb_srEtyC8dq_d0Ut4rnNU9YXyWpWspkGB_641UFTXhHkQeXAQ",
   env: Deno.env.get("ENV")!,
 };
